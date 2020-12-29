c0510d3f08a8898904593122b1285a61f290c06d
Tom Kurth <tom@losthalo.org>
2020-12-28 20:57:31 -0600

    Day 03.

--- Main.fs
+++ Main.fs
@@ -4,6 +4,6 @@ open Advent
 
 [<EntryPoint>]
 let main argv =
-    printfn "%u" Day02.Part1.answer.Value
-    printfn "%u" Day02.Part2.answer.Value
+    printfn "%A" Day03.Part1.answer.Value
+    printfn "%u" Day03.Part2.answer.Value
     0 // return an integer exit code
